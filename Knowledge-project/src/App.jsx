9cd17e0ce5fa4fd98a5982971703e7bae33c24ce
“Rabeya <rabeyarupa7815@gmail.com>
2023-09-16 11:25:17 +0600

    Create Fake Data for the blogs and load data

---
+++ Knowledge-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Blogs from './components/blogs/blogs'
+function App() {
+  return (
+    <>
+     <Header></Header>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
